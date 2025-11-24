25d3a154620d99df4ff70f184f1751d92f082bab
Isaac Jordan kimsey <74661612+jordankimsey@users.noreply.github.com>
2025-11-24 14:27:23 -0500

    update footer

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template: `<div class="flex justify-center">
+  template: `<div class="flex justify-center dark:text-neutral-300">
     Challenge by &nbsp;
     <a
+      class="dark:color-blue-300 hover:underline"
       style="color: hsl(228, 45%, 44%)"
       href="https://www.frontendmentor.io?ref=challenge"
       >Frontend Mentor</a
-    >. Coded by &nbsp;<a href="#">Jordan Kimsey</a>.
+    >. Coded by &nbsp;<a
+      href="https://github.com/jordankimsey"
+      target="_blank"
+      class="hover:underline dark:color-blue-300"
+      style="color: hsl(228, 45%, 44%)"
+      >Jordan Kimsey</a
+    >.
   </div>`,
 })
 export class Footer {}
